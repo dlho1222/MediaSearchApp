575e87784330930748d2334b62b0d136a25e846f
LEE HO <dlho1222@naver.com>
2024-11-03 20:01:50 +0900

    xml 작업

--- app/src/main/java/com/part3/mediasearch/SearchActivity.kt
+++ app/src/main/java/com/part3/mediasearch/SearchActivity.kt
@@ -1,20 +1,33 @@
 package com.part3.mediasearch
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.part3.mediasearch.databinding.ActivitySearchBinding
 
-class MainActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
+    private val binding: ActivitySearchBinding by lazy {
+        ActivitySearchBinding.inflate(
+            layoutInflater
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search_main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
     }
 }
