72121d1ffd5487e7c71c951f456024cc33e86c30
LEE HO <dlho1222@naver.com>
2025-02-07 22:52:11 +0900

    di refactoring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.part3.mediasearch"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.part3.mediasearch"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -48,14 +48,14 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
-    implementation("io.coil-kt:coil:2.4.0")
+    implementation("io.coil-kt:coil:2.7.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    implementation("com.google.dagger:hilt-android:2.52")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
     implementation("androidx.fragment:fragment-ktx:1.8.5")
-    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.52")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/part3/mediasearch/di/RetrofitModule.kt
+++ app/src/main/java/com/part3/mediasearch/di/RetrofitModule.kt
@@ -10,6 +10,7 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -17,30 +18,37 @@ import javax.inject.Singleton
 object RetrofitModule {
     @Provides
     @Singleton
+    @Named("BASE_URL")
+    fun provideWebAPI(): String = "https://dapi.kakao.com/v2/search/"
+
+    @Provides
+    @Singleton
     fun providesRetrofit(
+        @Named("BASE_URL") baseUrl: String,
         client: OkHttpClient.Builder,
         converterFactory: GsonConverterFactory
     ): Retrofit =
         Retrofit.Builder()
-            .baseUrl("https://dapi.kakao.com/v2/search/")
+            .baseUrl(baseUrl)
             .client(client.build())
             .addConverterFactory(converterFactory)
             .build()
 
     @Provides
     @Singleton
-    fun providesOkHttpClient(apiKey:String): OkHttpClient.Builder
-    = OkHttpClient.Builder()
+    fun providesOkHttpClient(apiKey: String): OkHttpClient.Builder = OkHttpClient.Builder()
         .addInterceptor(HeaderInterceptor(apiKey))
-        .connectTimeout(5,TimeUnit.SECONDS)
-        .readTimeout(5,TimeUnit.SECONDS)
-        .writeTimeout(5,TimeUnit.SECONDS)
+        .connectTimeout(5, TimeUnit.SECONDS)
+        .readTimeout(5, TimeUnit.SECONDS)
+        .writeTimeout(5, TimeUnit.SECONDS)
 
     @Provides
     @Singleton
-    fun providesConverterFactory():GsonConverterFactory=
-        GsonConverterFactory.create(GsonBuilder()
-            .setLenient()
-            .setDateFormat("yyyy-MM-dd'T':HH:mm:ss.SSSXXX")
-            .create())
+    fun providesConverterFactory(): GsonConverterFactory =
+        GsonConverterFactory.create(
+            GsonBuilder()
+                .setLenient()
+                .setDateFormat("yyyy-MM-dd'T':HH:mm:ss.SSSXXX")
+                .create()
+        )
 }
